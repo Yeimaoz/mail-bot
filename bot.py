58b9e9af8a84e44d36b6f4ed90ff4e4c98efa5a4
Yeimaoz <ny8859884@gmail.com>
2019-09-22 20:54:11 +0800

    [modify] process arguments

--- bot.py
+++ bot.py
@@ -1,13 +1,21 @@
 import smtplib
 from email.mime.text import MIMEText
 from argparse import ArgumentParser
+import sys
+import re
 
-def announce_lab_scores(sheet):
-    pass
+def announce_lab_scores(sheet, receiver_mail_server='cc.ncu.edu.tw'):
+    receiver_list = []
+    with open(sheet, 'r') as f:
+        receiver_list = f.readlines()
+    print(receiver_list)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("-m", "--mode", dest="mode")
-    parser.add_argument("-lss", "--lab-score-sheet", dest="lab_score_sheet")
+    parser.add_argument("-m", "--mode", dest="mode", required=True, 
+        help='mode flag is required')
+    parser.add_argument("-lss", "--lab-score-sheet", dest="lab_score_sheet", required='announce_lab_scores' in sys.argv,
+        help='lab_score_sheet is required if the specified mode is announce_lab_scores ...')
     args = vars(parser.parse_args())
-    print(args)
+    if args['mode'] == 'announce_lab_scores':
+        announce_lab_scores(args['lab_score_sheet'])
